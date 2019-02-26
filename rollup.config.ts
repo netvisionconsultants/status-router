625bfc05156611d15ec9dd7b1bce812f507b7ac1
Kyle Miracle <kyle.miracle@nvc-inc.net>
2019-02-26 13:05:49 -0500

    exclude fs

--- rollup.config.ts
+++ rollup.config.ts
@@ -16,7 +16,7 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['express'],
+  external: ['express', 'fs'],
   watch: {
     include: 'src/**',
   },
